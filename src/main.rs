568af122b10d67d73865dff8d70c2eb603564628
Danilo Bargen <mail@dbrgn.ch>
2017-04-02 18:01:08 +0200

    Switch from Flask to Rocket

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+use std::collections::HashMap;
+use std::io;
+use std::path::{Path, PathBuf};
+
+use rocket::response::NamedFile;
+
+#[get("/")]
+fn index() -> io::Result<NamedFile> {
+    NamedFile::open("static/index.html")
+}
+
+#[get("/static/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index, files]).launch();
+}
--- start.sh
+++
@@ -1,5 +0,0 @@
-export FLASK_APP=server.py
-export FLASK_DEBUG=1
-export SECRET_KEY=debug
-export LD_LIBRARY_PATH=.
-flask run
